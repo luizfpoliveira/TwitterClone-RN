9bd9c028a86bee0e336ede70dba40e4ee282bb5d
luiz Oliveira <luizfpeoliveira@gmail.com>
2024-03-12 12:07:21 -0300

    tweet component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,21 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import { TweetType } from "@/types";
+import { Entypo, EvilIcons } from "@expo/vector-icons";
+
+type iconButtonProps = {
+  icon: React.ComponentProps<typeof EvilIcons>["name"];
+  text?: string | number;
+};
+
+const IconButton = ({ icon, text }: iconButtonProps) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <EvilIcons name={icon} size={22} color={"gray"} />
+      <Text style={{ fontSize: 12, color: "gray" }}>{text}</Text>
+    </View>
+  );
+};
 
 type TweetProps = {
   tweet: TweetType;
@@ -11,8 +26,27 @@ const Tweet = ({ tweet }: TweetProps) => {
     <View style={styles.container}>
       <Image src={tweet.user.image} style={styles.profileImage} />
       <View style={styles.mainContainer}>
-        <Text style={styles.name}>{tweet.user.name}</Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.name}>{tweet.user.name}</Text>
+          <Text style={styles.username}>@{tweet.user.username} • 2h</Text>
+          <Entypo
+            name="dots-three-horizontal"
+            size={16}
+            color={"gray"}
+            style={{ marginLeft: "auto" }}
+          />
+        </View>
         <Text style={styles.content}>{tweet.content}</Text>
+
+        {tweet.image && <Image src={tweet.image} style={styles.image} />}
+
+        <View style={styles.footer}>
+          <IconButton icon="comment" text={tweet.numberOfComments} />
+          <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+          <IconButton icon="heart" text={tweet.numberOfLikes} />
+          <IconButton icon="chart" text={tweet.impressions || 0} />
+          <IconButton icon="share-apple" />
+        </View>
       </View>
     </View>
   );
@@ -43,5 +77,20 @@ const styles = StyleSheet.create({
   content: {
     lineHeight: 20,
     marginTop: 5,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 16 / 9,
+    marginVertical: 10,
+    borderRadius: 15,
+  },
+  username: {
+    color: "gray",
+    marginLeft: 5,
+  },
+  footer: {
+    flexDirection: "row",
+    marginVertical: 5,
+    justifyContent: "space-between",
   },
 });
